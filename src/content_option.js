6a1d95edf20111f02ab29469d0cc824595f298a7
Parul-0520 <100shparul@gmail.com>
2025-06-21 10:27:01 +0530

    Initial clean commit

---
+++ src/content_option.js
@@ -0,0 +1,154 @@
+const logotext = "PARUL SHARMA";
+const meta = {
+    title: "Parul Sharma",
+    description: "I’m Parul Sharma, a FrontEnd Web Developer and a ML Enthusiast, thriving at the intersection of technology, creativity, and problem-solving.",
+};
+
+const introdata = {
+    title: "I’m Parul Sharma",
+    animated: {
+        first: "FrontEnd Web Developer and ML Enthusiast",
+        second: "Driven by the joy of turning ideas into responsive front-end experiences and building smart ML models.",
+        
+    },
+    description: "Exploring technology with curiosity, building with intent, and growing through every challenge.",
+    your_img_url: "portfolio_picture.jpeg",
+};
+
+const dataabout = {
+    title: "About My Self",
+    aboutme: <p>
+    <em>From curiosity to creation— I embrace every challenge as a chance to grow.</em>
+    <br /><br/>
+    I am Parul Sharma a passionate developer who turns ideas into smart, seamless digital solutions. Whether I’m designing dynamic front-end interfaces or building machine learning models, I love working at the crossroads of innovation, creativity, and problem-solving.
+  </p> 
+};
+const worktimeline = [{
+        jobtitle: "Head Coordinator",
+        where: "Team Horizon, AKGEC",
+        date: "2025-26",
+    },
+    {
+       
+        jobtitle: "Campus Ambassador",
+        where: "GirlScript Summer of Code",
+        date: "2024",
+    },
+    {
+        jobtitle: "Data Analytics & Visualisation",
+        where: "Accenture (Virtual Internship)",
+        date: "2024",
+    },
+];
+
+const skills = [
+    {
+        name: "HTML, CSS",
+        value: 99,
+    },
+    {
+        name: "React",
+        value: 95,
+    },
+    {
+        name: "Tailwind, Bootstrap",
+        value: 95,
+    },
+    {
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 85,
+    },
+    {
+        name: "JavaScript",
+        value: 80,
+    },
+    {
+        name: "Git, Github",
+        value: 80,
+    },
+    {
+        name: "Postman",
+        value: 70,
+    },
+    {
+        name: "Scrum",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Mindset",
+        description: "Curious, growth-driven, & detail-oriented",
+    },
+    {
+        title: "Work Style",
+        description: "User-focused, iterative, & collaborative.",
+    },
+    {
+        title: "Core Values",
+        description: "Creativity, empathy, & persistence",
+    },
+];
+
+const dataportfolio = [{
+        img: "portfolio.png",
+        description: "Link to the portfolio",
+        link: "#",
+    },
+    {
+        img: "londonwebsite.png",
+        description: "Link to the website",
+        link: "#",
+    },
+    // {
+    //     img: "londonwebsite.png",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "100shparul@gmail.com",
+    YOUR_FONE: "+91 9971071152",
+    description: "Have a project in mind, a question to ask, or just want to connect? I'm always open to meaningful conversations, collaborations, or new opportunities in tech. Let’s turn ideas into impact — feel free to reach out!",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Parul-0520",
+    linkedin: "https://www.linkedin.com/in/parul-sharma-006251288/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
